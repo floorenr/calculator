31ff20e86e21d231cf682ca1845f7b25c658662e
Rene Flooren <floorenr@gmail.com>
2021-05-15 14:58:07 +0200

    note deleted

--- script.js
+++ script.js
@@ -129,14 +129,6 @@ function evaluate() {
 }
 
 // Gotchas: watch out for and fix these bugs if they show up in your code:
-// - Users should be able to string together several operations and get the right 
-//    answer, with each pair of numbers being evaluated at a time. For example, 
-//    12 + 7 - 5 * 3 = should yield 42. An example of the behavior we’re looking 
-//    for would be this student solution. Your calculator should not evaluate 
-//    more than a single pair of numbers at a time. If you enter a number then 
-//    an operator and another number that calculation should be displayed if 
-//    your next input is an operator. The result of the calculation should be 
-//    used as the first number in your new calculation.
 // - You should round answers with long decimals so that they don’t overflow the screen.
 // - Pressing = before entering all of the numbers or an operator could cause problems!
 // - Pressing “clear” should wipe out any existing data.. make sure the user is really 
